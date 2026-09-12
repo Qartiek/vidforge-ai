e3566b1d0b425a10d151779cbd709322666eedcf
Qartiek <thisiskrishnaprasad978@gmail.com>
2026-09-12 15:08:19 +0530

    Make pipeline idempotency migration recoverable

--- prisma/migrations/20260911000200_pipeline_job_idempotency/migration.sql
+++ prisma/migrations/20260911000200_pipeline_job_idempotency/migration.sql
@@ -1,4 +1,4 @@
 -- Make pipeline child-job creation idempotent by tying each child to its parent job.
-ALTER TABLE "Job" ADD COLUMN "previousJobId" TEXT;
+ALTER TABLE "Job" ADD COLUMN IF NOT EXISTS "previousJobId" TEXT;
 
-CREATE UNIQUE INDEX "Job_previousJobId_type_key" ON "Job"("previousJobId", "type");
+CREATE UNIQUE INDEX IF NOT EXISTS "Job_previousJobId_type_key" ON "Job"("previousJobId", "type");
